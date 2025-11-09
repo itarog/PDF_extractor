624d57e96edb8310623767dcbe2151d8d059e29e
Itamar Wallwater <itarog108@gmail.com>
2025-11-09 09:44:41 +0200

    updated get_page_text to consider each page dimensions

--- text_processing/init_processing.py
+++ text_processing/init_processing.py
@@ -20,14 +20,24 @@ def get_norm_bbox(bbox, max_height, max_width): # adjust to (x, y, width, height
     return norm_bbox
 
 
-def get_page_text(pdf_page, page_num, max_width=250):
+def get_page_text(pdf_page, page_num, max_width=250, dpi=200):
     output_list = []
     text_page = pdf_page.get_textpage()
     page_blocks = text_page.extractBLOCKS()
-    
+
+    zoom = dpi / 72.0
+    mat = fitz.Matrix(zoom, zoom)
+
+    pix = pdf_page.get_pixmap(matrix=mat)
+    page_w, page_h = pix.width, pix.height
+
     for x0, y0, x1, y1, line_text, line_num, _ in page_blocks:
+        x0 *= zoom
+        y0 *= zoom
+        x1 *= zoom
+        y1 *= zoom
         edited_text = clean_text(line_text)
-        norm_bbox = get_norm_bbox((y0, x0, y1, x1), 792, 612)
+        norm_bbox = get_norm_bbox((y0, x0, y1, x1), page_h, page_w) # 792, 612
 
         if len(edited_text) > max_width:
             wrapped_lines = textwrap.wrap(edited_text, width=max_width)
@@ -52,6 +62,39 @@ def get_page_text(pdf_page, page_num, max_width=250):
     ]
     
     return final_output_list
+
+# def get_page_text(pdf_page, page_num, max_width=250):
+#     output_list = []
+#     text_page = pdf_page.get_textpage()
+#     page_blocks = text_page.extractBLOCKS()
+    
+#     for x0, y0, x1, y1, line_text, line_num, _ in page_blocks:
+#         edited_text = clean_text(line_text)
+#         norm_bbox = get_norm_bbox((y0, x0, y1, x1), 792, 612)
+
+#         if len(edited_text) > max_width:
+#             wrapped_lines = textwrap.wrap(edited_text, width=max_width)
+#             for i, wrapped in enumerate(wrapped_lines):
+#                 output_list.append((
+#                     get_multi_idx(page_num, line_num + i),
+#                     wrapped,
+#                     norm_bbox
+#                 ))
+#         else:
+#             output_list.append((
+#                 get_multi_idx(page_num, line_num),
+#                 edited_text,
+#                 norm_bbox
+#             ))
+
+#     temp_output_list = sorted(output_list, key=lambda x: x[2][0])
+
+#     final_output_list = [
+#         (get_multi_idx(page_num, new_line_num), text, bbox)
+#         for new_line_num, (multi_idx, text, bbox) in enumerate(temp_output_list)
+#     ]
+    
+#     return final_output_list
 
 
 # def get_page_text(pdf_page, page_num):
